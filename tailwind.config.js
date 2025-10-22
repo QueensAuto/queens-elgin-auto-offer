3ed915dfeb81927f67e9fc0d5476acc812e3897a
Queens Auto <queensmobile11@gmail.com>
2025-10-22 10:57:35 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html" // Scan this file for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
